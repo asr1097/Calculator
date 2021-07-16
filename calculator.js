24216d223c2e0ec5012420481d8f9d2d389e3643
Milan <jmilan.97@gmail.com>
2021-07-16 17:37:08 +0200

    basic stuff

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
+let number1 = undefined;
+let number2 = undefined;
+let operator = undefined;
+let switchNumbers = false;
+let displayContent = document.querySelector('.display-content'); 
+
 function add (a, b) {
     return a + b;
 };
@@ -23,8 +29,29 @@ function operate (operator, number1, number2) {
     }
 };
 
-document.querySelectorAll('.num-btn').forEach(btn => btn.addEventListener('click', function() {
+document.querySelectorAll('.num-btn').forEach(btn => btn.addEventListener('click', function(){
+    if (switchNumbers) {
+        displayContent.textContent = "";
+        switchNumbers = false;};
     let numberPressed = this.textContent;
-    let displayContent = document.querySelector('.display-content'); 
     displayContent.textContent += numberPressed;
 }))
+
+document.querySelectorAll('.operator').forEach(op => op.addEventListener('click', function(){
+   operator = this.dataset.operator;
+   number1 = parseInt(displayContent.textContent, 10);
+   if (number1) {switchNumbers = true};
+}));
+
+document.querySelector('.enter').addEventListener('click', function (){
+    number2 = parseInt(displayContent.textContent, 10);
+    let result = operate(operator, number1, number2);
+    displayContent.textContent = result;
+    number1 = result;});
+
+document.querySelector('.clear').addEventListener('click', function(){
+    displayContent.textContent = "";
+    number1 = undefined;
+    number2 = undefined;
+    operator = undefined;
+})

d5ec28c1aaba3a87f4a423ae68658a6192bf194c
Milan <jmilan.97@gmail.com>
2021-07-16 16:28:29 +0200

    css and html

--- calculator.js
+++ calculator.js
@@ -13,3 +13,18 @@ function divide (a, b) {
 function multiply (a, b) {
     return a * b;
 };
+
+function operate (operator, number1, number2) {
+    switch (operator) {
+        case '+': return add(number1, number2);
+        case '-': return subtract(number1, number2);
+        case '/': return divide(number1, number2);
+        case '*': return multiply(number1, number2);
+    }
+};
+
+document.querySelectorAll('.num-btn').forEach(btn => btn.addEventListener('click', function() {
+    let numberPressed = this.textContent;
+    let displayContent = document.querySelector('.display-content'); 
+    displayContent.textContent += numberPressed;
+}))
